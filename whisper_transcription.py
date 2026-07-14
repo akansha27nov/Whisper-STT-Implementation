974019bea37ca0c3ca73105faf59b9a9c9860cd4
akansha27nov <akansha27nov@gmail.com>
2026-07-14 10:05:32 +0200

    Transcribe without chunking

--- whisper_transcription.py
+++ whisper_transcription.py
@@ -9,14 +9,18 @@ from dotenv import load_dotenv
 from pathlib import Path
 from openai import OpenAI
 
+# ======================================================
+# Step 1: Setting Up and Loading api key
+# ======================================================
 load_dotenv()
-
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+# ======================================================
+# Step 2: Verify downloaded file is accessible
+# ======================================================
+AUDIO_DATA_DIR = Path("audio")
 def verify_audio_file(file_path_str):
     file_path = Path(file_path_str)
-
-    # 1. Check if file exists
     if not file_path.exists():
         print(f"Error: The file '{file_path}' does not exist.")
         return False
@@ -27,5 +31,28 @@ def verify_audio_file(file_path_str):
 
     print(f"File found at: {file_path.absolute()}")
 
-audio_path = "audio/CA138clip.mp3"
+audio_path = AUDIO_DATA_DIR / "CA138clip.mp3"
 verify_audio_file(audio_path)
+
+# ======================================================
+# Step 3: Basic Transcription (Without Chunking)
+# ======================================================
+def transcribe_audio_file_as_is():
+    
+    with open(audio_path, "rb") as audio_file:
+        print("🤖 Transcribing with Whisper...")
+        transcript = client.audio.transcriptions.create(
+            model="whisper-1",
+            file=audio_file
+        )
+    return transcript
+
+transcription_text = transcribe_audio_file_as_is()
+print("\n📝 Transcription:")
+print("-" * 40)
+print(transcription_text.text)
+print("-" * 40)
+
+# ======================================================
+# Step 4: Transcription with Prompts (Guided Approach)
+# ======================================================
